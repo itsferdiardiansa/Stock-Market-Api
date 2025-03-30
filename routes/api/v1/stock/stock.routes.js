7cfd3a2e08af0cf004a1f8c7d94549b895b81fd1
itsferdiardiansa <ferdiardiansa@gmail.com>
2025-03-30 20:00:15 +0700

    chore: updated detail info on package.json

--- routes/api/v1/stock/stock.routes.js
+++ routes/api/v1/stock/stock.routes.js
@@ -1,31 +1,11 @@
 const express = require("express")
-const { fetchData } = require("@utils/fetcher")
-const { createResponse } = require("@utils/response")
 const validate = require("@middlewares/validate")
 const { searchSymbolSchema } = require("./stock.validate")
+const { getMostActives, getSearchSymbol } = require("@controllers/stock.controller")
 
 const router = express.Router()
 
-router.get("/most-actives", async (_, res) => {
-  try {
-    const response = await fetchData("most-actives")
-
-    res.json(createResponse(200, response))
-  } catch (error) {
-    res.json(createResponse(error.status, error))
-  }
-})
-
-router.get("/search-symbol", validate(searchSymbolSchema), async (req, res) => {
-  const { query } = req.query
-
-  try {
-    const response = await fetchData("search-symbol", { query })
-
-    res.json(createResponse(200, response))
-  } catch (error) {
-    res.json(createResponse(error.status, error))
-  }
-})
+router.get("/most-actives", getMostActives)
+router.get("/search-symbol", validate(searchSymbolSchema), getSearchSymbol)
 
 module.exports = router
