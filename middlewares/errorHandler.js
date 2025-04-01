797b81f5b87ea23ca60aa5e25bc19dffde10e84c
itsferdiardiansa <ferdiardiansa@gmail.com>
2025-04-01 16:07:40 +0700

    chore: added code linter

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const { createResponse } = require("../utils/response")
 
-const errorHandler = (err, _, res, next) => {
+const errorHandler = (err, _, res) => {
   res.json(createResponse(500, {
     body: {
       message: "Internal Server Error"
